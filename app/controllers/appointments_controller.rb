8f3d97f55ba7106527dc0106b00d7dc10d986238
joaquinrizzi <joaquinsotorizzi@gmail.com>
2024-09-06 14:17:57 +0200

    adding actions edit and delete to services and appointments

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -2,18 +2,14 @@ class AppointmentsController < ApplicationController
   before_action :authenticate_user!
   before_action :authorize_admin
 
-  before_action :set_appointment, only: [:show, :edit, :update, :destroy]
-  before_action :set_clients, only: [:index]
-  before_action :set_services, only: [:index]
-  before_action :set_appointments, only: [:index, :create]
+  before_action :set_appointment, only: %i[show edit update destroy]
+  before_action :set_clients, only: %i[index edit]
+  before_action :set_services, only: %i[index edit]
+  before_action :set_appointments, only: %i[index create]
 
   def index
     @appointment = Appointment.new
   end
-
-  # def new
-  #   @appointment = Appointment.new
-  # end
 
   def show
   end
@@ -23,38 +19,37 @@ class AppointmentsController < ApplicationController
     @appointment.user = current_user # or any other method to set the user
 
     if @appointment.save
-      redirect_to appointments_path, notice: 'Appointment was successfully created.'
+      redirect_to appointments_path, notice: 'Rdv créé avec succès.'
     else
-      flash.now[:alert] = 'Appointment was not created.'
+      flash.now[:alert] = 'Cet rdv n\'a pas été créé.'
       render :index
     end
   end
 
   def edit
+    @appointment = Appointment.find(params[:id])
   end
 
   def update
     if @appointment.update(appointment_params)
-      redirect_to appointments_path, notice: 'Appointment was successfully updated.'
+      redirect_to appointments_path, notice: 'Rdv modifié avec succès.'
     else
-      flash.now[:alert] = 'Appointment was not updated.'
+      flash.now[:alert] = 'Cet rdv n\'a pas été modifié.'
       render :edit
     end
-
   end
 
   def destroy
-    if @appointment.destroy
-      redirect_to appointments_path, notice: 'Appointment was successfully deleted.'
-    else
-      redirect_to appointments_path, alert: 'Appointment could not be deleted.'
-    end
+    @appointment = Appointment.find(params[:id])
+    @appointment.destroy
+    flash[:notice] = 'Le rdv a été suprimé avec succès.'
+    redirect_to appointments_path, status: :see_other
   end
 
   private
 
   def authorize_admin
-    redirect_to(root_path, alert: 'Access Denied') unless current_user.has_role?(:admin)
+    redirect_to(root_path, alert: 'Access refusé') unless current_user.has_role?(:admin)
   end
 
   def appointment_params
